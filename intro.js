e1a09f08fda82c7f0cf9eef683cd17ff7fb70f83
Omimakinde Clement <clementomimakinde@gmail.com>
2026-03-14 00:04:38 +0100

    second commit

---
+++ intro.js
@@ -0,0 +1,7 @@
+const intro = () =>{ 
+myName= 'omimakinde Clement';
+myAge = '17 yrs old';
+myFavProgrammingLang = 'Python';
+ console.log(`My name is ${myName}. I'm ${myAge} and my favourite programing language is ${myFavProgrammingLang}`);}
+
+ intro();
